a998311d9117be805b3e7210d43fc8d979e2dbd9
Yuvaraj S <ys+gwre@guidewire.com>
2025-12-05 12:00:47 +0530

    feat: initialized the repo

---
+++ kilikili/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/meyuviofficial/kilikili/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ kilikili/utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"os"
+)
+
+func makeDirectory(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return os.Mkdir(dir, 0755)
+	}
+	return nil
+}
